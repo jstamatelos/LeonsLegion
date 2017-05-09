76772cc4e309a097b343f400a1ae21e1f7341169
DanielPrahl <danielprahl@zipcoder11.local>
2017-05-09 16:09:52 -0400

    "added concrete subclasses of game managers"

--- src/main/java/com/leonslegion/casino/GameManager.java
+++ src/main/java/com/leonslegion/casino/GameManager.java
@@ -3,10 +3,19 @@ package com.leonslegion.casino;
 /**
  * Created by danielprahl on 5/9/17.
  */
-public class GameManager {
+public abstract class GameManager {
+    AccountManager accountManager;
 
     public GameManager(){
         // todo
+    }
+
+    public GameManager(AccountManager accountManager) {
+        this.accountManager = accountManager;
+    }
+
+    public AccountManager getAccountManager(){
+        return accountManager;
     }
 
 }
---
+++ src/main/java/com/leonslegion/casino/PokerGameManager.java
@@ -0,0 +1,17 @@
+package com.leonslegion.casino;
+
+/**
+ * Created by danielprahl on 5/9/17.
+ */
+public class PokerGameManager extends GameManager {
+
+    public PokerGameManager(){
+        // no-arg constructor
+    }
+
+    public PokerGameManager(AccountManager accountManager){
+        super(accountManager);
+        // todo
+    }
+
+}
