18ffccb672af18aff3d0615c287575e39330a590
cameronsima <cjsima@gmail.com>
2017-05-09 14:14:43 -0400

    Empty Hand, PokerHand, WarHand, BlackjackHand classes added

---
+++ src/main/java/com/leonslegion/casino/BlackjackHand.java
@@ -0,0 +1,15 @@
+package com.leonslegion.casino;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+/**
+ * Created by cameronsima on 5/9/17.
+ */
+public class BlackjackHand extends Hand implements Comparable {
+
+
+    public int compareTo(Object o) {
+        return 0;
+    }
+}
---
+++ src/main/java/com/leonslegion/casino/WarHand.java
@@ -0,0 +1,13 @@
+package com.leonslegion.casino;
+
+import java.util.Comparator;
+
+/**
+ * Created by cameronsima on 5/9/17.
+ */
+public class WarHand extends Hand implements Comparator {
+
+    public int compare(Object o1, Object o2) {
+        return 0;
+    }
+}
