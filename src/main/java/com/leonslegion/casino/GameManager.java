b4b24bf8456a65ecd61d882208772fbcfef240db
DanielPrahl <danielprahl@zipcoder11.local>
2017-05-10 13:26:00 -0400

    "corrected lobby issues and added methods to gamemanager"

--- src/main/java/com/leonslegion/casino/GameManager.java
+++ src/main/java/com/leonslegion/casino/GameManager.java
@@ -1,35 +1,44 @@
 package com.leonslegion.casino;
 
+import java.util.ArrayList;
+
 /**
  * Created by danielprahl on 5/9/17.
  */
 
 public abstract class GameManager {
+    private ArrayList<Player> players;
 
-
-
-    AccountManager accountManager;
+    @Deprecated
+    public GameManager(AccountManager accountManager){
+        this();
+    }
 
     public GameManager(){
-        this.accountManager = new AccountManager();
+        players = new ArrayList<>(16);
     }
 
-
-    public GameManager(AccountManager accountManager) {
-        this.accountManager = accountManager;
-    }
-
-    public AccountManager getAccountManager() {
-        return accountManager;
-    }
-
-    // dummy implementation, needs correction
+    // dummy implementation, override in subclass
     public void playGame(){
         System.out.println("Playing game....");
+        System.out.println(".");
+        System.out.println("..");
+        System.out.println("...");
         System.out.println("Game over, returning to lobby.");
         System.out.println("");
     }
 
+    public ArrayList<Player> getPlayers(){
+        return players;
+    }
+
+    public void addPlayer(Player p){
+        players.add(p);
+    }
+
+    public void removePlayer(Player p){
+        players.remove(p);
+    }
 
 }
 
--- src/main/java/com/leonslegion/casino/Lobby.java
+++ src/main/java/com/leonslegion/casino/Lobby.java
@@ -5,10 +5,7 @@ package com.leonslegion.casino;
  */
 public class Lobby {
     public boolean isRunning;
-    private AccountManager accountManager;
     private GameManager gameManager;
-    private Account loggedInAccount;
-    private InputHandler input;
 
 
     public Lobby(){
@@ -22,67 +19,83 @@ public class Lobby {
 
     public void initLobby(){
         isRunning = true;
-        accountManager =  new AccountManager();
         gameManager = null;
-        loggedInAccount = null;
-        input = new InputHandler();
     }
 
+    // this is the main game loop
     public void startLobby(){
         while(isRunning){
             System.out.println("!! !! !! !! !! !! !! !! !! !!");
             System.out.println("Welcome to the Casino Lobby!");
-            if(loggedInAccount == null) {
-                logInOrCreate();
-            }else {
-                selectGame();
-            }
+            actionSelection();
+            selectGame();
+
         }
     }
 
-    public void logIn(){
-        String loginName = input.getStringInput("Please enter a valid name");
-        loggedInAccount = accountManager.findAccount(loginName);
+    public void actionSelection(){
+        String selection = InputHandler.getStringInput("Would you like to play a game 'y' or 'n' ");
+        if(selection.equalsIgnoreCase("y")){
+            //do nothing, cascade down to selectGame
+        }else if(selection.equalsIgnoreCase("n")){
+            createAccountSelection();
+        }else{
+            System.out.println("selection unrecognized");
+            actionSelection();
+        }
+    }
+
+    public void createAccountSelection(){
+        String selection = InputHandler.getStringInput("Would you like to create a new account? 'y' or 'n' ");
+        if(selection.equalsIgnoreCase("y")){
+            createAccount();
+            createAccountSelection();
+        }else if(selection.equalsIgnoreCase("n")){
+            actionSelection();
+        }else{
+            System.out.println("selection unrecognized");
+            createAccountSelection();
+        }
     }
 
     public void createAccount(){
-        AccountFactory accountFactory = new AccountFactory();
-        String newName = input.getStringInput("What is your name?");
-        accountManager.addAccount(accountFactory.getAccountHolderName(newName));
-        loggedInAccount = accountManager.findAccount(newName);
+        String newName = InputHandler.getStringInput("What is your name?");
+        Account newAccount = AccountFactory.createAccountWithName(newName);
+        AccountManager.addAccount(newAccount);
+        System.out.println("Account has been created for '" + newName + "' with the ID: " + newAccount.getId());
     }
 
     public void selectGame(){
         System.out.println("Which game would you like to play? Please select a number.");
         System.out.println("Poker = 1, Blackjack = 2, War = 3, Roulette = 4, Slots = 5");
-        int selectedGame = input.getIntInput("Enter 0 to exit.");
+        int selectedGame = InputHandler.getIntInput("Enter 0 to exit.");
         switch (selectedGame){
             case 0:
                 exit();
                 break;
 
             case 1:
-                gameManager = new PokerGameManager(accountManager);
+                gameManager = new PokerGameManager();
                 gameManager.playGame();
                 break;
 
             case 2:
-                gameManager = new BlackjackGameManager(accountManager);
+                gameManager = new BlackjackGameManager();
                 gameManager.playGame();
                 break;
 
             case 3:
-                gameManager = new WarGameManager(accountManager);
+                gameManager = new WarGameManager();
                 gameManager.playGame();
                 break;
 
             case 4:
-                gameManager = new RouletteGameManager(accountManager);
+                gameManager = new RouletteGameManager();
                 gameManager.playGame();
                 break;
 
             case 5:
-                gameManager = new SlotGameManager(accountManager);
+                gameManager = new SlotGameManager();
                 gameManager.playGame();
                 break;
 
@@ -90,35 +103,6 @@ public class Lobby {
                 System.out.println(selectedGame + " is not a valid selection, please try again.");
                 selectGame();
         }
-    }
-
-    public void logInOrCreate(){
-        System.out.println("Please continue to the Casino Registration Office.");
-        System.out.println("Enter 1 to login to an existing account.");
-        System.out.println("Enter 2 to create a new account.");
-        int selection = input.getIntInput("Enter 0 to exit.");
-        switch (selection){
-            case 0:
-                exit();
-                break;
-
-            case 1:
-                logIn();
-                break;
-
-            case 2:
-                createAccount();
-                break;
-
-            default:
-                System.out.println(" is not a valid selection, please try again.");
-                logInOrCreate();
-                break;
-        }
-    }
-
-    public void viewRules(){
-        // todo
     }
 
     public void exit(){
