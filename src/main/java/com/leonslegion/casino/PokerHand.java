403f40a8c52875624f63be52ee47e8790be42527
Dan Zygmund-Felt <danzygmund-felt@zipcoder10.fios-router.home>
2017-05-09 22:53:02 -0400

    Created several helper methods for the setHandType method. Haven't accounted for the rank of the HandType, eg, what rank the triplets are in a three of a kind.

--- src/main/java/com/leonslegion/casino/PokerHand.java
+++ src/main/java/com/leonslegion/casino/PokerHand.java
@@ -1,5 +1,6 @@
 package com.leonslegion.casino;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
@@ -28,6 +29,87 @@ public class PokerHand extends Hand implements Comparable {
 
     void setHandType() {
         this.handType = HandType.HIGHCARD;
+        /*
+          helper methods below.
+          first step will be to find
+          the total number of ranks
+          present.
+          that will determine how to
+          proceed.
+          this way doesn't provide for
+          tie-breakers.
+        */
+    }
+
+    boolean hasFlush() {
+        Card.Suit suit = getHand().get(0).getSuit();
+        for(Card c : getHand()) {
+            if(c.getSuit() != suit) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    boolean hasStraight() {
+        Card.Rank rank = getHand().get(0).getRank();
+        for(int i = 1; i < 5; i++) {
+            rank++;
+            if(getHand().get(i).getRank() != rank) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    int countRank(Card.Rank rank) {
+        int count = 0;
+        for(Card c : getHand()) {
+            if(c.getRank() == rank) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    int numberOfRanksPresent() {
+        ArrayList<Card.Rank> ranks = new ArrayList<Card.Rank>();
+        for(Card c : getHand()) {
+            if(!ranks.contains(c.getRank())) {
+                ranks.add(c.getRank());
+            }
+        }
+        return ranks.size();
+    }
+
+    HandType fiveRankHandChooser() {
+        if(hasStraight()) {
+            if(hasFlush()) {
+                return HandType.STRAIGHTFLUSH;
+            } else {
+                return HandType.STRAIGHT;
+            }
+        } else if(hasFlush()) {
+            return HandType.FLUSH;
+        } else {
+            return HandType.HIGHCARD;
+        }
+    }
+
+    HandType fourRankHandChooser() {
+        return HandType.PAIR;
+    }
+
+    HandType threeRankHandChooser() {
+        //todo
+    }
+
+    HandType twoRankHandChooser() {
+        Card.Rank rank = getHand().get(0).getRank();
+        if(getHand().get(1).getRank() == rank) {
+            return HandType.FULLHOUSE;
+        }
+        return HandType.FOUROFAKIND;
     }
 
 }
