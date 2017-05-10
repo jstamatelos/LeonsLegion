9cc820e440ff96ee8f8f24517343050eaff17fd8
Dan Zygmund-Felt <danzygmund-felt@zipcoder10.local>
2017-05-10 16:09:39 -0400

    Made fixes to Deck.

--- src/main/java/com/leonslegion/casino/Deck.java
+++ src/main/java/com/leonslegion/casino/Deck.java
@@ -4,39 +4,34 @@ import java.util.*;
 
 public class Deck {
 
-    private Deck() {}
+    ArrayList<Card> deck;
 
-    public static ArrayList<Card> createNewDeck() {
-        ArrayList<Card> newDeck = new ArrayList<Card>();
+    private Deck() {
         for (Card.Rank rank: Card.Rank.values()) {
             for (Card.Suit suit: Card.Suit.values()) {
-                newDeck.add(new Card(rank, suit));
+                deck.add(new Card(rank, suit));
             }
 
         }
-        return newDeck;
     }
 
 
-
-    public static String printDeckToString (ArrayList<Card> cardDeck) {
-        String deckString = (cardDeck.get(0)).getRank().toString() + " of " + (cardDeck.get(0)).getSuit().toString();
-        for (int i = 1; i < cardDeck.size(); i++) {
-            deckString += ", " + (cardDeck.get(i)).getRank().toString() + " of " + (cardDeck.get(i)).getSuit().toString();
+    public String toString () {
+        String deckString = (deck.get(0)).getRank().toString() + " of " + (deck.get(0)).getSuit().toString();
+        for (int i = 1; i < deck.size(); i++) {
+            deckString += ", " + (deck.get(i)).getRank().toString() + " of " + (deck.get(i)).getSuit().toString();
         }
         return deckString;
     }
 
 
 
-    public static void shuffleDeck (ArrayList<Card> cardDeck) {Collections.shuffle(cardDeck);}
+    public void shuffleDeck () {Collections.shuffle(deck);}
 
 
 
-    public static Card dealCard(ArrayList<Card> cardDeck) {
-        Card aCard = cardDeck.get(0);
-        cardDeck.remove(0);
-        return aCard;
+    public Card dealCard() {
+        return deck.remove(deck.size() - 1);
     }
 
 
