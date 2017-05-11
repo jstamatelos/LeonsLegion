711394600ba7efa3df671e6eec16c793b7bd70ba
Sarah Weisser <sarahweisser@zipcoder23.local>
2017-05-11 13:23:22 -0400

    added spin method to reel class and test

--- src/test/java/com/leonslegion/casino/TestReel.java
+++ src/test/java/com/leonslegion/casino/TestReel.java
@@ -1,8 +1,24 @@
 package com.leonslegion.casino;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by sarahweisser on 5/11/17.
  */
 public class TestReel {
 
+    @Test
+    public void testSpin() {
+
+        //given
+        Reel reel1 = new Reel();
+
+        //when
+        String actualResult = reel1.spinReel();
+
+        //then
+        Assert.assertTrue(reel1.getImages().contains(actualResult));
+
+    }
 }
