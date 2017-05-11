67323d13bae9165cb3694091970b219af1b3ce19
Sarah Weisser <sarahweisser@zipcoder23.local>
2017-05-11 15:36:33 -0400

    added slotplayer class to place bets and tested

--- src/main/java/com/leonslegion/casino/SlotGame.java
+++ src/main/java/com/leonslegion/casino/SlotGame.java
@@ -3,11 +3,10 @@ package com.leonslegion.casino;
 /**
  * Created by jarrydstamatelos on 5/9/17.
  */
-public class SlotGame implements Spin {
-    // Method to spin the slot wheel
-    public String spin() {
-        return "";
-    }
+public class SlotGame {
+
+
+
     // Player bets
     // Player spins
     // Depending on outcome of spin, player gets payout or loses bet amount
--- src/main/java/com/leonslegion/casino/SlotPlayer.java
+++ src/main/java/com/leonslegion/casino/SlotPlayer.java
@@ -1,7 +1,32 @@
 package com.leonslegion.casino;
 
+import java.util.ArrayList;
+
 /**
  * Created by sarahweisser on 5/11/17.
  */
-public class SlotPlayer {
+public class SlotPlayer extends Player {
+
+    private double bet = 5;
+
+    public SlotPlayer (double balance, long accountId) {
+        super(balance, accountId);
+    }
+
+    public double getBet() {
+        return bet;
+    }
+
+    public double placeBet(double bet) throws Exception {
+        if (super.getBalance() < 5) {
+            System.out.println("It is $5.00 to play this machine.");
+            System.out.println("You do not have enough in your account to play.");
+        }
+        else {
+            this.setBalance(this.getBalance() - 5);
+        }
+        return 0;
+    }
+
+
 }
