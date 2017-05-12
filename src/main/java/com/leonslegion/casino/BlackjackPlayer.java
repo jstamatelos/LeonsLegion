519ff89d72b298948cd47f4ab159fc523307dfcc
cameronsima <cjsima@gmail.com>
2017-05-12 08:49:17 -0400

    blackjack

--- src/main/java/com/leonslegion/casino/BlackjackPlayer.java
+++ src/main/java/com/leonslegion/casino/BlackjackPlayer.java
@@ -1,5 +1,7 @@
 package com.leonslegion.casino;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class BlackjackPlayer extends CardPlayer {
 
     BlackjackPlayer(double balance, long accountId) {
@@ -12,5 +14,23 @@ public class BlackjackPlayer extends CardPlayer {
 
     void stay() {
         //nothing happens. not sure if this needs a method.
+    }
+
+    public String placeBet (String bet) {
+        if (!NumberUtils.isParsable(bet)) {
+            String newBet = InputHandler.getStringInput("That's not a valid bet.");
+            return placeBet(newBet);
+        }
+        else if (Double.parseDouble(bet) < 0) {
+            String newBet = InputHandler.getStringInput("You cannot make a negative bet!");
+            return placeBet(newBet);
+        }
+        else if (getBalance() - Double.parseDouble(bet) < 0) {
+            String newBet = InputHandler.getStringInput("Your bet is greater than your balance!");
+            return placeBet(newBet);
+        }
+        else {
+            return bet;
+        }
     }
 }
