9ba25e3ee604829740c682683b98328b57761d94
Jarryd Stamatelos <jarryd.stamatelos@gmail.com>
2017-05-13 18:59:51 -0400

    Minor war changes

--- src/main/java/com/leonslegion/casino/CardGamePackage/WarGame.java
+++ src/main/java/com/leonslegion/casino/CardGamePackage/WarGame.java
@@ -12,41 +12,39 @@ import java.util.Comparator;
  */
 
 public class WarGame extends CardGame implements Comparator {
-    
 
-    static Deck dealerDeck = new Deck();
-    static Deck playerDeck = new Deck();
 
-    static Card playerCard;
-    static Card dealerCard;
+    private static Deck dealerDeck = new Deck();
+    private static Deck playerDeck = new Deck();
+
+    private static Card playerCard;
+    private static Card dealerCard;
 
 
     /**
      * Method called from the lobby to start the game
      */
 
-    public static void startWarGame(){
-
+    public static void startWarGame() {
         setUp();
         gameLogic();
-
     }
 
 
     /**
      * Handles game logic
-     *  Dealer showing / Player showing card / comparison / result / exit
+     * Dealer showing / Player showing card / comparison / result / exit
      */
 
-    private static void gameLogic(){
+    private static void gameLogic() {
 
         WarPlayer newWarPlayer = createWarPlayer();
         boolean playRound = true;
         while (playRound) {
             double bet = WarGame.placeBet(newWarPlayer);
-            Console.println(setDealerCard());
-            Console.println(setplayerCard());
-            Console.println(determineWinner(newWarPlayer, bet));
+            setDealerCard();
+            setplayerCard();
+            determineWinner(newWarPlayer, bet);
             playRound = WarGame.exit();
         }
 
@@ -57,7 +55,7 @@ public class WarGame extends CardGame implements Comparator {
      * Display for game start
      */
 
-    private static void setUp(){
+    private static void setUp() {
 
         Console.println("WAR! WHAT IS IT GOOD FOR!");
         Console.printDashes();
@@ -68,6 +66,7 @@ public class WarGame extends CardGame implements Comparator {
 
     /**
      * Creates player, tests that ID is valid, will not accept an invalid ID
+     *
      * @return
      */
 
@@ -92,12 +91,13 @@ public class WarGame extends CardGame implements Comparator {
 
     /**
      * Places bet, 1 bet per round
+     *
      * @param warPlayer player that is linked with the entered ID at game start
      * @return places bet
      */
 
     private static double placeBet(WarPlayer warPlayer) {
-        double bet = InputHandler.getDoubleInput("Please place a bet: ");
+        double bet = Console.getDoubleInput("Please place a bet: ");
         long accountID = warPlayer.getAccount().getId();
         for (int account = 0; account < Account.AccountManager.getAccounts().size(); account++) {
             if (Account.AccountManager.getAccounts().get(account).getId() == accountID) {
@@ -105,7 +105,7 @@ public class WarGame extends CardGame implements Comparator {
                 if (balance == 0) {
                     Console.println("You have a balance of 0!");
                     Console.println("Sorry! You are out of money.");
-
+                    WarGame.exit();
                     return 0;
                 }
                 if (bet > balance) {
@@ -123,47 +123,52 @@ public class WarGame extends CardGame implements Comparator {
 
     /**
      * Dealers card
+     *
      * @return displays dealers card
      */
 
-    private static String setDealerCard(){
+    private static void setDealerCard() {
         dealerDeck.shuffleDeck();
         dealerCard = dealerDeck.dealCard();
-        return "Dealer draws a : " + dealerCard.toString();
+        Console.println("Dealer draws a : " + dealerCard.toString());
+        //return "Dealer draws a : " + dealerCard.toString();
     }
 
     /**
      * players card
+     *
      * @return displays players card
      */
-    private static String setplayerCard(){
+    private static void setplayerCard() {
         playerDeck.shuffleDeck();
         playerCard = playerDeck.dealCard();
-        return "Player draws a : " + playerCard.toString();
+        Console.println("Player draws a : " + playerCard.toString());
+       // return "Player draws a : " + playerCard.toString();
     }
 
 
     /**
      * Compares dealercard / playercard / determines high card
+     *
      * @param warPlayer
-     * @param bet player bet - doubled if 'War' condition met
+     * @param bet       player bet - doubled if 'War' condition met
      * @return win / loss / draw message
      */
-    public static String determineWinner(WarPlayer warPlayer, double bet){
-        if (playerCard.getPointValue() > dealerCard.getPointValue()){
+    private static void determineWinner(WarPlayer warPlayer, double bet) {
+        if (playerCard.getPointValue() > dealerCard.getPointValue()) {
             long accountID = warPlayer.getAccount().getId();
             for (int account = 0; account < Account.AccountManager.getAccounts().size(); account++) {
                 if (Account.AccountManager.getAccounts().get(account).getId() == accountID) {
                     double balance = Account.AccountManager.getAccounts().get(account).getAccountBalance();
                     Account.AccountManager.getAccounts().get(account).setAccountBalance(bet);
-                    Console.print("Your balance is now: ");
+                    Console.println("Your balance is now: ");
                     Console.println("$ %,.2f", Account.AccountManager.getAccounts().get(account).getAccountBalance());
                     Console.printDashes();
-
                 }
             }
-            return "You win! Nice!";
-        } else if (playerCard.getPointValue() < dealerCard.getPointValue()){
+            Console.println("You win! Nice!");
+
+        } else if (playerCard.getPointValue() < dealerCard.getPointValue()) {
             long accountID = warPlayer.getAccount().getId();
             for (int account = 0; account < Account.AccountManager.getAccounts().size(); account++) {
                 if (Account.AccountManager.getAccounts().get(account).getId() == accountID) {
@@ -174,17 +179,18 @@ public class WarGame extends CardGame implements Comparator {
                     Console.printDashes();
                 }
             }
-            return "Dealer wins! Oh well.";
+            Console.println( "Dealer wins! Oh well.");
         } else {
             Console.println("Tie! WAR!!!!!! Your bet is doubled!!!!\n Another Card is pulled for each player!");
-            Console.println(setDealerCard());
-            Console.println(setplayerCard());
-            return determineWinner(warPlayer, bet * 2);
+            setDealerCard();
+            setplayerCard();
+            determineWinner(warPlayer, bet * 2);
         }
     }
 
     /**
      * Allows exit back to menu
+     *
      * @return
      */
     private static boolean exit() {
@@ -193,9 +199,10 @@ public class WarGame extends CardGame implements Comparator {
             return false;
         } else if (exitOpportunity.equalsIgnoreCase("stay")) {
             return true;
-        } else {return exit();}
+        } else {
+            return exit();
+        }
     }
-
 
 
     public int compare(Object o1, Object o2) {
@@ -207,11 +214,6 @@ public class WarGame extends CardGame implements Comparator {
 
     @Override
     public void initialDeal() {
-       //
-    }
-
-    @Override
-    public void setHasDealer(boolean hasDealer) {
-        super.setHasDealer(true);
+        //
     }
 }
