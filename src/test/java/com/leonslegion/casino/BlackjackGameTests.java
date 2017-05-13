34190294871d6d4e8cacfc22864627f5f1ed790f
cameronsima <cjsima@gmail.com>
2017-05-13 15:29:37 -0400

    blackjackgametest

--- src/test/java/com/leonslegion/casino/BlackjackGameTests.java
+++ src/test/java/com/leonslegion/casino/BlackjackGameTests.java
@@ -14,14 +14,13 @@ import org.junit.Test;
  */
 public class BlackjackGameTests {
 
-    BlackjackPlayer sam;
-    BlackjackPlayer bob;
+    BlackjackPlayer player;
     BlackjackGame newGame;
 
     @Before
     public void setUp() {
         Account acct = Account.AccountFactory.createAccountWithName("Cameron");
-        BlackjackPlayer player = new BlackjackPlayer(acct);
+        player = new BlackjackPlayer(acct);
 
         newGame = new BlackjackGame(player);
 
@@ -34,38 +33,5 @@ public class BlackjackGameTests {
 
         System.out.println(newGame);
     }
-/*
-    @Test
-    public void testPromptString() {
 
-        BlackjackHand hand = new BlackjackHand();
-        BlackjackHand dealerHand = new BlackjackHand();
-        Card card1 = new Card(Card.Rank.TEN, Card.Suit.CLUBS);
-        Card card2 = new Card(Card.Rank.EIGHT, Card.Suit.DIAMONDS);
-        hand.addCard(card1);
-        hand.addCard(card2);
-        dealerHand.addCard(card1);
-        dealerHand.addCard(card2);
-
-        BlackjackGame game = new BlackjackGame();
-        game.initialDeal();
-
-        String actualOutput = game.buildUserPromptString(hand, dealerHand);
-
-        String expectedOutput = "Your hand: TEN of CLUBS; EIGHT of DIAMONDS; \n" +
-                "is worth 18 points. Hit or stay?";
-
-        Assert.assertEquals(expectedOutput, actualOutput);
-    }
-*/
-    @Test
-    public void testInitialDeal() {
-
-
-    }
-
-    @Test
-    public void testCheck21() {
-
-    }
 }
